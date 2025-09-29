007545613d1688865ec32493f1a8df681c87db97
V3\victo <vcantero06@colegiosantamonica.eu>
2025-09-29 12:18:56 +0200

    second commit

--- index.js
+++ index.js
@@ -9,42 +9,72 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.use(cookieParser());
 
+const isAuth = (req, res, next) => {
+  if (req.cookies && req.cookies.user) {
+    return next();
+  }
+  res.redirect('/login');
+};
+
 app.get('/', (req, res) => {
     //sql
   res.render('index',{title: "Super Plantilla",name: "Victor"})
 })
 
-isAuth = (req,res, next) => {
-  if (req.cookies && req.cookies.user){
+isadmin = (req,res, next) => {
+  if (req.cookies && req.cookies.role === "admin"){
       return next();
   }
-  res.redirect('/login')
+  res.status(403).send("Acceso denegado: solo admins");
 }
 
-// gestion de la visita
+isUser = (req, res, next) => {
+  if (req.cookies && req.cookies.role === "user") {
+    return next();
+  }
+  res.status(403).send("Acceso denegado: solo usuarios");
+};
+
+
+
+// Gestion de la visita
+
 app.get('/login', (req, res) => {
   res.render('login')
 })
 
 app.get('/logout', (req, res) => {
   res.clearCookie("user");
+  res.clearCookie("role");
   res.redirect('login');
-})
-app.post('/login', (req,res) => {
-    const {user,password} = req.body;
-    //console.log(req.body)
+});
 
-   if (user == "admin" && password == "12345"){
-    //console.log("usuario y contraseña correcta")
-    res.cookie("user", user,) //opciones - js no secure si
-    res.redirect('home')
-   }else {
+app.post('/login', (req,res) => {
+    const { admin, password1, user, password } = req.body;
+    console.log(req.body)
+
+   if (admin == "admin" && password1 == "12345"){
+    console.log("usuario y contraseña correcta")
+    res.cookie("user", admin,) //opciones - js no secure si
+    res.cookie("role", "admin");
+    res.redirect('admin')
+
+   } else if(user == "user" && password == "1234"){
+    res.cookie("user", user,) 
+    res.cookie("role", "user");
+    res.redirect('profile')
+   }
+   else {
     res.status(401).redirect("login")
    }
 });
 
-app.get('/home',isAuth, (req, res) => {
-  res.render('home')
+app.get('/profile', isAuth, isUser, (req, res) => {
+  res.render('profile');
+});
+
+app.get('/admin', isAuth, isadmin, (req, res) => {
+  res.render('admin');
 });
 
 app.listen(port, () => {
